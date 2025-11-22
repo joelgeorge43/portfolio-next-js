877adc26af72b4de26cd9a290cd144e926af814a
Joel George Jacob <118953319+joelgeorge43@users.noreply.github.com>
2025-11-22 11:20:38 +0530

    Update content.tsx
    Started updating basic profile details like Profile Name and things like that in content.tsx

--- src/resources/content.tsx
+++ src/resources/content.tsx
@@ -2,14 +2,14 @@ import { About, Blog, Gallery, Home, Newsletter, Person, Social, Work } from "@/
 import { Line, Logo, Row, Text } from "@once-ui-system/core";
 
 const person: Person = {
-  firstName: "Selene",
-  lastName: "Yu",
-  name: `Selene Yu`,
-  role: "Design Engineer",
-  avatar: "/images/avatar.jpg",
-  email: "example@gmail.com",
-  location: "Asia/Jakarta", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
-  languages: ["English", "Bahasa"], // optional: Leave the array empty if you don't want to display languages
+  firstName: "Joel",
+  lastName: "George",
+  name: `Joel George`,
+  role: "UX UI Designer",
+  avatar: "/images/dp.jpg",
+  email: "joelgeorge43@gmail.com",
+  location: "Asia/Kolkata", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
+  languages: ["English", "Hindi", "Malayalam", "Tamil"], // optional: Leave the array empty if you don't want to display languages
 };
 
 const newsletter: Newsletter = {
@@ -29,7 +29,7 @@ const social: Social = [
   {
     name: "LinkedIn",
     icon: "linkedin",
-    link: "https://www.linkedin.com/company/once-ui/",
+    link: "https://www.linkedin.com/in/jooeeyy/",
   },
   {
     name: "Threads",
@@ -39,7 +39,7 @@ const social: Social = [
   {
     name: "Email",
     icon: "email",
-    link: `mailto:${person.email}`,
+    link: `mailto:joelgeorge43@gmail.com`,
   },
 ];
 
@@ -65,7 +65,7 @@ const home: Home = {
   },
   subline: (
     <>
-      I'm Selene, a design engineer at{" "}
+      I'm Joel, a UX UI Designer at{"Sanse Solutions Gen Trading LLC."}
       <Logo
         dark
         icon="/trademarks/wordmark-dark.svg"
@@ -98,7 +98,7 @@ const about: About = {
     title: "Introduction",
     description: (
       <>
-        Selene is a Jakarta-based design engineer with a passion for transforming complex challenges
+        Joel is a Kochi-based UX UI Designer and Front-end Developer with a passion for transforming complex challenges
         into simple, elegant design solutions. Her work spans digital interfaces, interactive
         experiences, and the convergence of design and technology.
       </>
